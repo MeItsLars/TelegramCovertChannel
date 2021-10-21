5414dfdac3476b291bd79f13d74e6be4a7813fd2
Lars Jeurissen <38491967+MeItsLars@users.noreply.github.com>
2021-10-21 12:04:19 +0200

    Initial version. When running, creates a new sticker pack.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+from telethon import TelegramClient, events, sync
 
+api_id = 8205533
+api_hash = 'e0750e29f8b159ac273dc072e0541e67'
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+client = TelegramClient('current', api_id, api_hash)
+client.start()
+
+client.send_message('@Stickers', '/newpack')
+client.send_message('@Stickers', 'SuperSecretCovertChannel')
+client.send_file('@Stickers', './pack_image.png', force_document=True)
+client.send_message('@Stickers', '😉')
+client.send_message('@Stickers', '/publish')
+client.send_message('@Stickers', '/skip')
+client.send_message('@Stickers', 'SuperSecretCovertChannel')
+
+print('Done!')
